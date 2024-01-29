d5d7b34143454662ff3fbd3051ac0fdc4ecb8f09
Ytter-by <q3075868@gmail.com>
2024-01-29 11:01:17 +0300

    написал программу на условия, которая должна проверять значение температуры.

---
+++ temp/temp.js
@@ -0,0 +1,12 @@
+let temperature = -1
+
+if (temperature > 30) {
+    console.log("Сегодня очень жарко, можно надеть легкую одежду");
+} else if (temperature > 20) {
+    console.log("Сегодня тепло, но лучше надеть теплую одежу");
+} else if (temperature < 10) {
+    console.log("Сегодня холодно, нужно надеть зимнюю одежду");
+} else {
+    console.log("Температура на улице комфортная, выбирайте, что носить");
+}
+  
